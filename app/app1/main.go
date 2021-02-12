0155b9704ed553c883a88da980a69b17e01a0f8b
Jyury11 <ryu050101@gmail.com>
2021-02-12 04:31:07 +0000

    feat: change

--- app/app1/main.go
+++ app/app1/main.go
@@ -1,6 +1,15 @@
 // main.go
 package main
 
+const Version = "1.2.3"
+const slug = "Jyury11/go-update"
+
 func main() {
 	println("Hello World!")
+
+	v := semver.MustParse(Version)
+	latest, err := selfupdate.UpdateSelf(v, slug)
+	if !latest.Version.Equals(v) {
+		errors.Wrap(err, "Binary update failed")
+	}
 }
